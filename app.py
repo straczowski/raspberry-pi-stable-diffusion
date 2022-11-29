7bc497b34737f68d4a1915dab65f5371ec3c4076
Raoul Straczowski <raoulstraczowski@ip-192-168-178-85.eu-central-1.compute.internal>
2022-11-29 14:47:12 +0100

    update prompt in code with readme

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 pipe = StableDiffusionPipeline.from_pretrained("../stable-diffusion-v1-5", low_cpu_mem_usage=True)
 pipe = pipe.to("cpu")
 
-prompt = "a red fox in space painted by Monet"
+prompt = "a black tree with golden leaves painted by Monet, autumn"
 image = pipe(prompt, num_inference_steps=31, width=400, height=400).images[0]
 
 image.save("output.png")
